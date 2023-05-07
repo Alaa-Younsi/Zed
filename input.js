90530dae300de81b173be4174f41d49a1ca3612e
Alaa Younsi <103404455+Alaa-Younsi@users.noreply.github.com>
2023-05-07 03:59:47 +0100

    Add files via upload

---
+++ input.js
@@ -0,0 +1,11 @@
+function returnInput(){
+  let name  = document.getElementById("Name").value;
+  let email = document.getElementById("Email").value;
+  let age = document.getElementById("Number").value;
+  let website = document.getElementById("Website").value;
+  alert(name, email);
+  alert(email);
+  alert(age);
+  alert(website);
+  alert("we got your infos, we will look you up and send you an email!");
+}
---
+++ main.js
@@ -0,0 +1,419 @@
+$(document).ready(function(){
+
+  $(".about-us").click(function(){
+      $(".fade").fadeOut(500);
+    });
+});
+
+$(document).ready(function(){
+
+  $(".about-us").click(function(){
+      $(".contact-us-text").fadeOut(500);
+    });
+});
+
+$(document).ready(function(){
+
+  $(".about-us").click(function(){
+      $(".about-us-text").fadeIn(500);
+    });
+});
+
+$(document).ready(function(){
+
+  $(".home").click(function(){
+      $(".about-us-text").fadeOut(500);
+    });
+});
+
+$(document).ready(function(){
+
+  $(".home").click(function(){
+      $(".contact-us-text").fadeOut(500);
+    });
+});
+
+$(document).ready(function(){
+
+  $(".home").click(function(){
+      $(".fade").fadeIn(500);
+    });
+});
+
+$(document).ready(function(){
+
+  $(".contact-us").click(function(){
+      $(".fade").fadeOut(500);
+    });
+});
+
+$(document).ready(function(){
+
+  $(".contact-us").click(function(){
+      $(".about-us-text").fadeOut(500);
+    });
+});
+
+$(document).ready(function(){
+
+  $(".contact-us").click(function(){
+      $(".contact-us-text").fadeIn(500);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Hide-button").click(function(){
+      $("#More").slideUp(500);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Show-button").click(function(){
+      $("#More").slideDown(500);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Developer").click(function(){
+      $(".fade").fadeOut(500);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Developer").click(function(){
+      $(".pill").fadeIn(500);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Red-pill").click(function(){
+      $("#Page").slideUp(1000);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Blue-pill").click(function(){
+      $(".pill").fadeOut(500);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Blue-pill").click(function(){
+      $(".fade").fadeIn(500);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Developer").click(function(){
+      $(".more-all").fadeOut(500);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Blue-pill").click(function(){
+      $(".more-all").fadeIn(500);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Red-pill").click(function(){
+      $(".pill").fadeOut(1);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Red-pill").click(function(){
+      $("#For").fadeOut(1);
+    });
+})
+
+$(document).ready(function(){
+
+$("#Designer").click(function(){
+    $(".fade").fadeOut(500);
+  });
+});
+
+$(document).ready(function(){
+
+$("#Designer").click(function(){
+    $(".pill").fadeIn(500);
+  });
+});
+
+$(document).ready(function(){
+
+  $("#Designer").click(function(){
+      $(".more-all").fadeOut(500);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Need-developer").click(function(){
+      $(".fade").fadeOut(500);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Need-developer").click(function(){
+      $(".more-all").fadeOut(500);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Need-developer").click(function(){
+      $("#Need-developer1").fadeIn(500);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Need-developer").click(function(){
+      $(".find").fadeIn(500);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Find1").click(function(){
+      $(".find").fadeOut(500);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Find1").click(function(){
+      $("#Need-developer").fadeOut(500);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Find1").click(function(){
+      $(".fade").fadeIn(500);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Find1").click(function(){
+      $(".more-all").fadeIn(500);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Need-designer").click(function(){
+      $(".fade").fadeOut(500);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Need-designer").click(function(){
+      $(".more-all").fadeOut(500);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Need-designer").click(function(){
+      $("#Need-developer1").fadeIn(500);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Need-designer").click(function(){
+      $(".find").fadeIn(500);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Find1").click(function(){
+      $(".find").fadeOut(500);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Find1").click(function(){
+      $(".fade").fadeIn(500);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Find1").click(function(){
+      $(".more-all").fadeIn(500);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Find2").click(function(){
+      $(".find").fadeOut(1);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Find2").click(function(){
+      $("#For").fadeOut(1);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Find2").click(function(){
+      $("#Page").slideUp(1000);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Email").click(function(){
+      $("#Email-animation").fadeIn(500);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Email").click(function(){
+      $("#Email").fadeOut(1);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Link4").click(function(){
+      $(".fade").fadeOut(500);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Link4").click(function(){
+      $(".about-us-text").fadeOut(500);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Link4").click(function(){
+      $(".contact-us-text").fadeOut(500);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Link6").click(function(){
+      $(".fade").fadeOut(500);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Link6").click(function(){
+      $(".about-us-text").fadeOut(500);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Link6").click(function(){
+      $(".contact-us-text").fadeOut(500);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Link3").click(function(){
+      $(".fade").fadeOut(500);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Link3").click(function(){
+      $(".about-us-text").fadeOut(500);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Link3").click(function(){
+      $(".contact-us-text").fadeOut(500);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Red-pill").click(function(){
+      $("#No-way-back").fadeIn(5000);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Red-pill").click(function(){
+      $("#Page2-button").fadeIn(5000);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Red-pill").click(function(){
+      $("#Title").fadeOut(1);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Red-pill").click(function(){
+      $("#c").fadeIn(5000);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Find2").click(function(){
+      $("#Title").fadeOut(1);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Find2").click(function(){
+      $("#Page2-button2").fadeIn(5000);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Find2").click(function(){
+      $("#c").fadeIn(5000);
+    });
+})
+
+$(document).ready(function(){
+
+  $("#Find2").click(function(){
+      $("#No-way-back").fadeIn(5000);
+    });
+})
